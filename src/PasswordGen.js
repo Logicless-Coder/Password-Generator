ba4528cb43db6fe9fb7e69d8696ca64fedab6bbd
Logicless-Coder <abhirathore565@gmail.com>
2020-06-10 19:19:54 +0530

    almost finished styling slider box and header

--- src/PasswordGen.js
+++ src/PasswordGen.js
@@ -22,16 +22,30 @@ class PasswordGen extends Component {
             type="text"
             placeholder="Generated password will be shown here"
           />
-          <div className="slider">
-            <label>{this.state.length}</label>
+          <div className="slider-box">
+            <label className="length-label">{this.state.length}</label>
             <Slider
+              className="slider"
               axis="x"
               xstep={1}
               xmin={1}
               xmax={20}
               x={this.state.length}
               onChange={({ x }) => this.setState({ length: parseInt(x) })}
+              styles={{
+                track: {
+                  backgroundColor: "rgba(163, 203, 56, 0.5)",
+                  width: 200,
+                  height: 20,
+                },
+                active: {
+                  backgroundColor: "rgba(0, 148, 50,1.0)",
+                },
+              }}
             />
+          </div>
+          <div className="conditions">
+            <input type="checkbox" text="a-z" />
           </div>
         </div>
       </div>
